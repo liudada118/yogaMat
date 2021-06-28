07cc7adc27f4bab2b9e4604aaf4c43ec0da6aba8
wzyblowfire <33181375+wzyblowfire@users.noreply.github.com>
2021-06-28 10:42:39 +0800

    稳定测试
    稳定测试

--- Appblue.js
+++ Appblue.js
@@ -16,18 +16,40 @@ import {
   LayoutAnimation,
   ToastAndroid
 } from 'react-native';
-
+let comnum = 1
+import { com } from './src/js/com'
 import BleManager from 'react-native-ble-manager';
 import { WebView } from 'react-native-webview';
 const { height, scale } = Dimensions.get('window');
+import Echarts from 'native-echarts';
+import Fili from 'fili'
+const arr = [289, 404, 452, 467, 474, 480, 485, 493, 495, 492, 497, 498, 500, 503, 499, 494, 493, 497, 500, 497, 501, 495, 498, 500, 494, 499, 499, 501, 496, 497, 493, 498, 500, 501, 502, 502, 503, 503, 502, 503, 502, 502, 503, 503, 504, 502, 503, 504, 503, 502, 505, 502, 503, 502, 500, 503, 504, 502, 502, 505, 504, 504, 503, 506, 504, 506, 505, 506, 507, 507, 506, 505, 507, 506, 506, 505, 504, 504, 506, 504, 505, 506, 506, 507, 504, 504, 504, 505, 506, 507, 505, 504, 504, 503, 502, 503, 504, 503, 501, 505, 510, 526, 537, 537, 535, 532, 531, 533, 535, 519, 519, 527, 536, 549, 561, 564, 562, 552, 543, 542, 548, 551, 555, 560, 561, 563, 563, 563, 563, 563, 566, 564, 563, 563, 561, 566, 561, 561, 559, 561, 559, 564, 564, 561, 560, 561, 557, 560, 559, 556, 556, 560, 561, 562, 563, 559, 560, 559, 557, 556, 557, 558, 561, 563, 563, 568, 567, 561, 556, 549, 553, 556, 558, 551, 542, 540, 537, 531, 499, 494, 490, 492, 489, 490, 485, 491, 495, 494, 505, 500, 502, 500, 498, 499, 494, 497, 473, 455, 398, 331, 342, 393, 423, 435, 461, 465, 471, 474, 476, 463, 459, 463, 467, 466, 478, 495, 485, 467, 450, 403, 444, 448, 459, 459, 469, 467, 456, 446, 452, 454, 456, 461, 463, 462, 452, 439, 401, 430, 451, 450, 457, 468, 453, 444, 443, 441, 452, 461, 470, 469, 471, 495, 488, 459, 451, 459, 475, 478, 476, 472, 460, 455, 455, 447, 444, 447, 454, 464, 510, 547, 553, 532, 509, 503, 542, 556, 545, 532, 538, 541, 546, 554, 552, 553, 558, 543, 517, 512, 509, 531, 543, 546, 554, 551, 544, 523, 538, 546, 537, 556, 558, 555, 550, 546, 545, 547, 548, 552, 551, 490, 437, 453, 479, 545, 552, 551, 546, 532, 525, 531, 548, 547, 537, 514, 484, 513, 518, 532, 531, 528, 511, 463, 372, 362, 381, 463, 507, 481, 485, 516, 536, 511, 441]
+var firCalculator = new Fili.FirCoeffs();
 
+// calculate filter coefficients
+var firFilterCoeffs = firCalculator.lowpass({
+  order: 20, // filter order
+  Fs: 10, // sampling frequency
+  Fc: 0.2 // cutoff frequency
+  // forbandpass and bandstop F1 and F2 must be provided instead of Fc
+});
+var firFilter = new Fili.FirFilter(firFilterCoeffs);
+firFilter.simulate(arr)
+// console.log(firFilter.simulate(arr), '++++++++++');
 /**
  * 适配宽度*/
 const myWidth = 300
 function Width(num) {
   return num * width / myWidth
 }
-
+let smooth = 0
+let dataArr = []
+let relArr = []
+let relArr1 = []
+let relArr2 = []
+let relArr3 = []
+let relArr4 = []
+let stable = []
 const width = Dimensions.get('window').width;
 const window = Dimensions.get('window');
 const BleManagerModule = NativeModules.BleManager;
@@ -53,7 +75,14 @@ export default class App extends React.Component {
       width5: 0,
       width6: 0,
       width7: 0,
-      text: ''
+      text: '',
+      aver: '',
+      isView: false,
+      option: '',
+      option1: '',
+      option2: '',
+      option3: '',
+      arr: ''
     };
     this.l1 = React.createRef();
     this.l2 = React.createRef();
@@ -283,7 +312,7 @@ export default class App extends React.Component {
         j++
       }
     }
-    console.log(valueArr, a)
+    // console.log(valueArr)
     LayoutAnimation.configureNext(LayoutAnimation.Presets.linear);
     this.setState({
       width: valueArr[0] * (width - 20) / 1020,
@@ -337,6 +366,206 @@ export default class App extends React.Component {
       })
     }
     if (valueArr[4] > 130) {
+
+      if (dataArr.length < 50) {
+        comnum++
+        dataArr.push(valueArr[4])
+        smooth = smooth + (com(dataArr) - smooth) / 10
+        // console.log(smooth,com(dataArr))
+        let fillArr = firFilter.simulate(dataArr)
+        relArr1.push(valueArr[4])
+        console.log(relArr1)
+        relArr2.push(fillArr)
+        relArr3.push(com(dataArr).toFixed(0))
+        // relArr4.push(relArr3)
+        relArr.push(com(fillArr).toFixed(0))//Number( com(fillArr).toFixed(0)))
+        let relvalue = relArr.reduce((prev ,cur) => parseInt(prev)  + parseInt(cur) , 0)
+        relArr4.push(relvalue / relArr.length)
+        // console.log(fillArr)
+        this.setState({
+          aver: com(fillArr).toFixed(0)
+        })
+
+
+
+        this.setState({
+          option: {
+            title: {
+              text: 'ECharts demo'
+            },
+            tooltip: {},
+            legend: {
+              data: ['销量']
+            },
+            xAxis: {
+              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [{
+              name: '销量',
+              type: 'bar',
+              data: relArr
+            }]
+          }, arr: relArr,
+          option1: {
+            title: {
+              text: '原始'
+            },
+            tooltip: {},
+            legend: {
+              data: ['销量']
+            },
+            xAxis: {
+              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [{
+              name: '销量',
+              type: 'bar',
+              data: relArr1
+            }]
+          },
+          option2: {
+            title: {
+              text: '滤波'
+            },
+            tooltip: {},
+            legend: {
+              data: ['销量']
+            },
+            xAxis: {
+              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [{
+              name: '销量',
+              type: 'bar',
+              data: relArr2
+            }]
+          },
+          option3: {
+            title: {
+              text: '平均滤波'
+            },
+            tooltip: {},
+            legend: {
+              data: ['销量']
+            },
+            xAxis: {
+              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [{
+              name: '销量',
+              type: 'bar',
+              data: relArr4
+            }]
+          },
+        })
+
+
+
+
+      } else {
+        comnum++
+        dataArr.shift()
+        dataArr.push(valueArr[4])
+        smooth = smooth + (com(dataArr) - smooth) / 10
+        // console.log(smooth,com(dataArr))
+        let fillArr = firFilter.simulate(dataArr)
+        
+        relArr1.push(valueArr[4])
+        console.log(relArr1)
+        let fillArr1 = firFilter.simulate(relArr1)
+        relArr2=fillArr1
+        relArr3.push(com(dataArr).toFixed(0))
+        relArr.push(com(fillArr).toFixed(0))
+        let relvalue = relArr.reduce((prev ,cur) =>parseInt(prev)  + parseInt(cur)  , 0)
+        relArr4.push(relvalue / relArr.length)
+        // console.log(fillArr)
+        this.setState({
+          aver: com(fillArr).toFixed(0)
+        })
+        // relArr.push(Number( com(dataArr).toFixed(0)))
+        //Number( com(fillArr).toFixed(0)))
+        // relArr.push(valueArr[4])
+        // console.log(relArr)
+        this.setState({
+          option: {
+            title: {
+              text: 'ECharts demo'
+            },
+            tooltip: {},
+            legend: {
+              data: ['销量']
+            },
+            xAxis: {
+              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [{
+              name: '销量',
+              type: 'bar',
+              data: relArr
+            }]
+          },
+          arr: relArr,
+          option1: {
+            title: {
+              text: '原始'
+            },
+            tooltip: {},
+            legend: {
+              data: ['销量']
+            },
+            xAxis: {
+              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [{
+              name: '销量',
+              type: 'bar',
+              data: relArr1
+            }]
+          },
+          option2: {
+            title: {
+              text: '滤波'
+            },
+            tooltip: {},
+            legend: {
+              data: ['销量']
+            },
+            xAxis: {
+              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [{
+              name: '销量',
+              type: 'bar',
+              data: relArr2
+            }]
+          }, option3: {
+            title: {
+              text: '平均滤波'
+            },
+            tooltip: {},
+            legend: {
+              data: ['销量']
+            },
+            xAxis: {
+              data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [{
+              name: '销量',
+              type: 'bar',
+              data: relArr4
+            }]
+          },
+        })
+
+      }
       this.l2.setNativeProps({
         style: {
           opacity: 1,
@@ -344,6 +573,69 @@ export default class App extends React.Component {
         }
       })
     }
+
+    // if(dataArr.length < 10){
+    //   dataArr.push(valueArr[4])
+    //   smooth = smooth + (com(dataArr) - smooth)/10
+    //   console.log(smooth,com(dataArr))
+    //   this.setState({
+    //     aver : smooth
+    //   })
+    //   relArr.push(com(dataArr))
+
+    //   this.setState({
+    //     option : {
+    //       title: {
+    //           text: 'ECharts demo'
+    //       },
+    //       tooltip: {},
+    //       legend: {
+    //           data:['销量']
+    //       },
+    //       xAxis: {
+    //           data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+    //       },
+    //       yAxis: {},
+    //       series: [{
+    //           name: '销量',
+    //           type: 'bar',
+    //           data: relArr
+    //       }]
+    //     },arr : relArr
+    //   })
+    // }else{
+    //   dataArr.shift()
+    //   dataArr.push(valueArr[4])
+    //   smooth = smooth + (com(dataArr) - smooth)/10
+    //   console.log(smooth,com(dataArr))
+    //   this.setState({
+    //     aver : smooth
+    //   })
+    //   relArr.push(com(dataArr))
+
+    //   this.setState({
+    //     option : {
+    //       title: {
+    //           text: 'ECharts demo'
+    //       },
+    //       tooltip: {},
+    //       legend: {
+    //           data:['销量']
+    //       },
+    //       xAxis: {
+    //           data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+    //       },
+    //       yAxis: {},
+    //       series: [{
+    //           name: '销量',
+    //           type: 'bar',
+    //           data: relArr
+    //       }]
+    //     },
+    //     arr : relArr
+    //   })
+
+    // }
     if (valueArr[5] > 130) {
       this.l1.setNativeProps({
         style: {
@@ -577,43 +869,52 @@ export default class App extends React.Component {
           source={{ uri: 'https://github.com/facebook/react-native' }}
           style={{ marginTop: 20 }}
         /> */}
-        <TouchableHighlight
-          style={{
-            marginTop: 40,
-            margin: 20,
-            padding: 20,
-            backgroundColor: '#ccc',
-          }}
-          onPress={() => this.startScan()}>
-          <Text>Scan Bluetooth ({this.state.scanning ? 'on' : 'off'})</Text>
+        <ScrollView style={{ flex: 1 }}>
+          <TouchableHighlight
+            style={{
+              marginTop: 40,
+              margin: 20,
+              padding: 20,
+              backgroundColor: '#ccc',
+            }}
+            onPress={() => this.startScan()}>
+            <Text>Scan Bluetooth ({this.state.scanning ? 'on' : 'off'})</Text>
 
-        </TouchableHighlight>
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <View style={{ width: Width(240), height: Width(540) > height * 0.8 ? 0.8 * height : Width(540), borderWidth: 1, borderColor: '#f9d6f8' }}>
-            <View ref={(e) => this.l8 = e} style={{ flex: 1, borderBottomWidth: 1, borderColor: '#f9d6f8', flexDirection: 'row', backgroundColor: 'rgb(36,4,49)', justifyContent: 'center', alignItems: 'center' }}>
-              <Text style={{ color: 'rgb(36,4,49)', fontSize: Width(30) }}>7</Text>
-            </View>
-            <View style={{ flex: 2, borderBottomWidth: 1, borderColor: '#f9d6f8', position: 'relative' }}>
-              <View ref={(e) => this.l3 = e} style={{ position: 'absolute', top: '50%', backgroundColor: 'rgb(36,4,49)', left: '50%', zIndex: 2, width: Width(120), height: Width(120), transform: [{ translateY: -Width(60) }, { translateX: -Width(60) }, { rotateZ: '45deg' }], borderWidth: 1, borderColor: '#f9d6f8', justifyContent: 'center', alignItems: 'center' }}>
-                <Text style={{ color: 'rgb(36,4,49)', fontSize: Width(30), transform: [{ rotateZ: '-45deg' }] }}>4</Text>
+
+          </TouchableHighlight>
+          {/* <> */}
+          < AppView option={this.state.option} arr={[this.state.arr]} />
+          < AppView option={this.state.option1} arr={[this.state.arr]} />
+          < AppView option={this.state.option2} arr={[this.state.arr]} />
+          < AppView option={this.state.option3} arr={[this.state.arr]} />
+          <Text style={{ textAlign: 'center', width: '100%' }}>{parseInt(this.state.aver)}</Text>
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', opacity: 0 }}>
+            <View style={{ width: Width(240), height: Width(540) > height * 0.8 ? 0.8 * height : Width(540), borderWidth: 1, borderColor: '#f9d6f8' }}>
+              <View ref={(e) => this.l8 = e} style={{ flex: 1, borderBottomWidth: 1, borderColor: '#f9d6f8', flexDirection: 'row', backgroundColor: 'rgb(36,4,49)', justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ color: 'rgb(36,4,49)', fontSize: Width(30) }}>7</Text>
               </View>
-              <View style={{ flex: 1, borderBottomWidth: 1, borderColor: '#f9d6f8', flexDirection: 'row' }}>
-                <View style={{ flex: 1, borderRightWidth: 1, borderColor: '#f9d6f8', backgroundColor: 'rgb(36,4,49)', justifyContent: 'center', alignItems: 'center' }} ref={(e) => this.l1 = e}><Text style={{ color: 'rgb(36,4,49)', fontSize: Width(30) }}>6</Text></View>
-                <View style={{ flex: 1, backgroundColor: 'rgb(36,4,49)', justifyContent: 'center', alignItems: 'center' }} ref={(e) => this.l2 = e}><Text style={{ color: 'rgb(36,4,49)', fontSize: Width(30) }}>5</Text></View>
+              <View style={{ flex: 2, borderBottomWidth: 1, borderColor: '#f9d6f8', position: 'relative' }}>
+                <View ref={(e) => this.l3 = e} style={{ position: 'absolute', top: '50%', backgroundColor: 'rgb(36,4,49)', left: '50%', zIndex: 2, width: Width(120), height: Width(120), transform: [{ translateY: -Width(60) }, { translateX: -Width(60) }, { rotateZ: '45deg' }], borderWidth: 1, borderColor: '#f9d6f8', justifyContent: 'center', alignItems: 'center' }}>
+                  <Text style={{ color: 'rgb(36,4,49)', fontSize: Width(30), transform: [{ rotateZ: '-45deg' }] }}>4</Text>
+                </View>
+                <View style={{ flex: 1, borderBottomWidth: 1, borderColor: '#f9d6f8', flexDirection: 'row' }}>
+                  <View style={{ flex: 1, borderRightWidth: 1, borderColor: '#f9d6f8', backgroundColor: 'rgb(36,4,49)', justifyContent: 'center', alignItems: 'center' }} ref={(e) => this.l1 = e}><Text style={{ color: 'rgb(36,4,49)', fontSize: Width(30) }}>6</Text></View>
+                  <View style={{ flex: 1, backgroundColor: 'rgb(36,4,49)', justifyContent: 'center', alignItems: 'center' }} ref={(e) => this.l2 = e}><Text style={{ color: 'rgb(36,4,49)', fontSize: Width(30) }}>5</Text></View>
+                </View>
+                <View style={{ flex: 1, flexDirection: 'row' }}>
+                  <View style={{ flex: 1, borderRightWidth: 1, borderColor: '#f9d6f8', backgroundColor: 'rgb(36,4,49)', justifyContent: 'center', alignItems: 'center' }} ref={(e) => this.l4 = e}><Text style={{ color: 'rgb(36,4,49)', fontSize: Width(30) }}>2</Text></View>
+                  <View style={{ flex: 1, backgroundColor: 'rgb(36,4,49)', justifyContent: 'center', alignItems: 'center' }} ref={(e) => this.l5 = e}><Text style={{ color: 'rgb(36,4,49)', fontSize: Width(30) }}>3</Text></View>
+                </View>
               </View>
               <View style={{ flex: 1, flexDirection: 'row' }}>
-                <View style={{ flex: 1, borderRightWidth: 1, borderColor: '#f9d6f8', backgroundColor: 'rgb(36,4,49)', justifyContent: 'center', alignItems: 'center' }} ref={(e) => this.l4 = e}><Text style={{ color: 'rgb(36,4,49)', fontSize: Width(30) }}>2</Text></View>
-                <View style={{ flex: 1, backgroundColor: 'rgb(36,4,49)', justifyContent: 'center', alignItems: 'center' }} ref={(e) => this.l5 = e}><Text style={{ color: 'rgb(36,4,49)', fontSize: Width(30) }}>3</Text></View>
+                <View style={{ flex: 1, borderRightWidth: 1, borderColor: '#f9d6f8', backgroundColor: 'rgb(36,4,49)', justifyContent: 'center', alignItems: 'center' }} ref={(e) => this.l6 = e}>
+                  <Text style={{ color: 'rgb(36,4,49)', fontSize: Width(30) }}>1</Text>
+                </View>
+                {/* <View style={{ flex: 1,backgroundColor : 'rgb(36,4,49)' }} ref={(e) => this.l7 = e}></View> */}
               </View>
-            </View>
-            <View style={{ flex: 1, flexDirection: 'row' }}>
-              <View style={{ flex: 1, borderRightWidth: 1, borderColor: '#f9d6f8', backgroundColor: 'rgb(36,4,49)', justifyContent: 'center', alignItems: 'center' }} ref={(e) => this.l6 = e}>
-                <Text style={{ color: 'rgb(36,4,49)', fontSize: Width(30) }}>1</Text>
-              </View>
-              {/* <View style={{ flex: 1,backgroundColor : 'rgb(36,4,49)' }} ref={(e) => this.l7 = e}></View> */}
             </View>
           </View>
-        </View>
+        </ScrollView>
       </View>
     );
   }
@@ -638,3 +939,42 @@ const styles = StyleSheet.create({
     margin: 10
   }
 });
+
+
+class AppView extends React.Component {
+  constructor() {
+    super()
+  }
+  shouldComponentUpdate(nextProps) {
+    // console.log(this.props.arr.length,relArr.length , '++++++++++++')
+    if (comnum % 30 == 0) {
+
+      return true
+    }
+    // console.log(nextProps)
+    return false
+  }
+  render() {
+    return <Echarts option={this.props.option} height={300} />
+  }
+}
+AppView.defaultProps = {
+  option: {
+    title: {
+      text: 'ECharts demo'
+    },
+    tooltip: {},
+    legend: {
+      data: ['销量']
+    },
+    xAxis: {
+      data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+    },
+    yAxis: {},
+    series: [{
+      name: '销量',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }]
+  }
+}
---
+++ src/js/com.js
@@ -0,0 +1,15 @@
+export function com(Arr){
+    let arr = []
+    Arr.forEach((a,index) => {
+        if(index >=15){
+            arr.push(a)
+        }
+    })
+    let average = arr.reduce((prev,cur) => prev + cur , 0)/arr.length
+    let res = 0 
+    arr.forEach((a,index) => {
+        res += (a - average)*(a - average)
+    })
+    // console.log(res)
+    return arr.length > 1 ? res/arr.length : 0
+}
